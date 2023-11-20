62bb32c5234b62c151b24f144840ba209ce081ec
prateekch33 <prateekchandra27@outlook.com>
2023-11-20 13:51:24 +0530

    Q3 Output comments added

--- Day 1/q3.java
+++ Day 1/q3.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Q3 {
 	public static void main(String[] args) {
+		System.out.printf("Table of 2^%d: \n",Integer.parseInt(args[0]));
 		for(int i=0;i<Integer.parseInt(args[0]);i++) {
 			System.out.println((int)Math.pow(2,i));
 		}
