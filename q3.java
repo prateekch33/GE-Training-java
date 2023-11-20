36eefe926e3c4cf1d22756b4fa7abb52b9e38e01
prateekch33 <prateekchandra27@outlook.com>
2023-11-20 13:41:27 +0530

    removed from root added to Day 1

--- q3.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-class Q3 {
-	public static void main(String[] args) {
-		for(int i=0;i<Integer.parseInt(args[0]);i++) {
-			System.out.println((int)Math.pow(2,i));
-		}
-	}
-}
